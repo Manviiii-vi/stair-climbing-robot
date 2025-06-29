9efea08e611c63bb307180d82d2bda788c215489
Manvi <manvijaglan4444@gmail.com>
2025-06-29 12:52:39 +0530

    Add placeholder section for N20 lift motor control in ESP32 code
    Added commented-out pin definitions and control logic for N20 motors to be used in the lifting mechanism. This prepares the structure for future integration of the vertical climb system.

---
+++ code/esp32_stair_bot.ino
@@ -0,0 +1,10 @@
+// Placeholder code structure for ESP32-based stair climbing robot
+
+void setup() {
+  Serial.begin(115200);
+  // Setup motor pins (to be filled later)
+}
+
+void loop() {
+  // Motor logic for climbing stairs (to be implemented)
+}
